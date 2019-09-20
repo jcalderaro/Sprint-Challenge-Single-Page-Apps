2205b80ada0b1804ff4d844a0679e37e4a2c1331
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-09-20 14:31:12 -0700

    Progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Header from "./components/Header.js";
+import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage";
+import { Route } from "react-router-dom";
+import SearchForm from "./components/SearchForm.js";
 
-
-export default function App() {
+function App() {
   return (
     <main>
       <Header />
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/characters" component={CharacterList} />
+      <SearchForm />
     </main>
   );
 }
+
+export default App;
